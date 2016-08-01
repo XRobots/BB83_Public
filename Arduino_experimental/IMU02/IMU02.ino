6a72b2339d08f62de684bd552e003499aab4280f
James Bruton <xrobots@gmail.com>
2016-08-01 09:36:13 +0100

    BNO055 IMU code

---
+++ Arduino_experimental/IMU02/IMU02.ino
@@ -0,0 +1,58 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+/* Set the delay between fresh samples */
+#define BNO055_SAMPLERATE_DELAY_MS (20)
+
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
+#define LED_PIN 13
+bool blinkState = false;
+
+float pitch;
+float roll;
+
+void setup(void)
+{
+  Serial.begin(115200);
+
+  /* Initialise the sensor */
+  if(!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+  delay(1000);
+  bno.setExtCrystalUse(true);
+  
+  pinMode(LED_PIN, OUTPUT);
+}
+
+
+void loop(void)
+{
+  /* Get a new sensor event */
+  sensors_event_t event;
+  bno.getEvent(&event);
+
+  pitch = (event.orientation.z);
+  roll = (event.orientation.y);  
+
+  Serial.print(pitch, 4);
+  Serial.print(",");
+  Serial.print(roll, 4);
+  Serial.print('\n'); 
+
+
+  /* Wait the specified delay before requesting nex data */
+  delay(BNO055_SAMPLERATE_DELAY_MS);
+
+    // blink LED to indicate activity
+  blinkState = !blinkState;
+  digitalWrite(LED_PIN, blinkState);
+
+        
+}
